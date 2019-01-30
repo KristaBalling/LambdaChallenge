0f9343240fc87228d5bd8a92d4421c4a2c9a390d
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-30 16:27:36 -0500

    challenges complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -127,6 +128,15 @@ public class Main {
                 .count();
 
         System.out.println("Number of names beginning with A is: " + namesBeginningWithA);
+
+        //challenge 14 - add a terminal operation to this chain so that the peek call will execute
+        topNames2015
+                .stream()
+                .map(name -> name.substring(0,1).toUpperCase() + name.substring(1))
+                .peek(System.out::println)
+                .sorted(String::compareTo)
+                .collect(Collectors.toList());
+
 
 
 
