f275a33bb5e6179db4332dfdcc13137247e24599
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-30 09:42:30 -0500

    challenge 1 complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -3,6 +3,27 @@ package com.theironyard;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //convert this class into a lambda
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                String myString = "Let's split this up into an array";
+                String[] parts = myString.split(" ");
+                for (String part: parts) {
+                    System.out.println(part);
+                }
+            }
+        };
+
+        //lambda version
+        Runnable runnable1 = () -> {
+            String myString = "Let's split this up into an array";
+            String[] parts = myString.split(" ");
+            for (String part: parts) {
+                System.out.println(part);
+            }
+        };
+
     }
 }
