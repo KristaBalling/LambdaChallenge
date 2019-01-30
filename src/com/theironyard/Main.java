21e8bf9b70590bb0f7ccc8ef93ec78023a596d94
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-30 16:07:07 -0500

    challenge 10 complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,8 @@
 package com.theironyard;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -79,6 +82,35 @@ public class Main {
         //to a variable called supplierResult, then print to the console
         String supplierResult = iLoveJava.get();
         System.out.println(supplierResult);
+
+        //challenge 9 - write code to print the items in the list in sorted order, and with the first letter
+        //in each name upper-cased. The name "harry" should be printed as "Harry" and should be printed after
+        //"Emily" and before "Isla". Use lambda expressions where possible
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        List<String> firstUpperCaseList = new ArrayList<>();
+        topNames2015.forEach(name ->
+                firstUpperCaseList.add(name.substring(0,1).toUpperCase() + name.substring(1)));
+//        firstUpperCaseList.sort((s1, s2) -> s1.compareTo(s2));
+//        firstUpperCaseList.forEach(s -> System.out.println(s));
+        firstUpperCaseList.sort(String::compareTo);
+        firstUpperCaseList.forEach(System.out::println);
+
+        //challenge 10 - change the code so that it uses method references.
+        //Remember that a method reference looks like Class::MethodName
+
+
 
 
 
